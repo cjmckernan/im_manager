bba4422453a2b0a43a872968f33247671b9666e1
cjmckernan <conorjmckernan@gmail.com>
2024-07-16 12:27:19 +0100

    added mixin to filter only companies payments

--- payments/models.py
+++ payments/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.conf import settings
 from im_users.models import ImCompany
+from inventory.mixins import CompanyFilterMixin
 
 
 class Payment(models.Model):
     transaction_id = models.CharField(max_length=100, unique=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,  null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
     company = models.ForeignKey(ImCompany, on_delete=models.DO_NOTHING)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
--- payments/views.py
+++ payments/views.py
@@ -1,10 +1,9 @@
 from .models import Payment
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from inventory.mixins import CompanyFilterMixin
 
-class PaymentListView(LoginRequiredMixin, ListView):
+
+class PaymentListView(LoginRequiredMixin, CompanyFilterMixin, ListView):
     model = Payment
     template_name = "payments/payment_list.html"
-
-
-
